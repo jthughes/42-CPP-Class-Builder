11decda290171d49780ed0a6e588c7d27e516bb5
Jeremy Hughes <jthughes@users.noreply.github.com>
2024-07-30 11:14:44 +0930

    Can add properties and methods to class with various options.
    Methods can't yet have any arguments, and properties aren't initialised or copied in constructors.

---
+++ BuilderTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace CPP_Class_Builder;
+
+public class MethodItem
+{
+    public string Name { get; set; } = "";
+    public string AccessLevel { get; set; } = "public";
+	public bool IsStatic { get; set; } = false;
+    public bool IsVirtual { get; set; } = false;
+    public bool IsPure { get; set; } = false;
+    public string ReturnType { get; set; } = "";
+	public bool IsConst { get; set; } = false;
+}
+
+public class PropertyItem
+{
+    public string Name { get; set; } = "";
+    public string Type { get; set; } = "";
+    public string DefaultValue { get; set; } = "";
+    public string AccessLevel { get; set; } = "private";
+    public bool IsStatic { get; set; } = false;
+    public bool IsConst { get; set; } = false;
+    public bool HasGetter { get; set; } = false;
+    public bool HasSetter { get; set; } = false;
+}
